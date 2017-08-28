16df669af30df88d5eec1ce5bb7f9c96617f658a
alvinyen <103356015@nccu.edu.tw>
2017-08-28 17:32:47 +0800

    complete preProcess and countTime functionality

--- task2.js
+++ task2.js
@@ -17,3 +17,46 @@
 // >300 => 150cents/min
 // 00:05:00 => Math.ceil(300)=5 => 150*5 = 750cents
 // 00:05:01 => Math.ceil(301)=6 => 150*6 = 900cents
+
+// 0<=hh<=99
+// 0<=mm/ss<=59
+
+function countTime (str) {
+    const secondsPerHour = 60 * 60;
+    const secondsPerMin = 60;
+    const times = str.split(':');
+
+    // ss -> times[2]
+    // mm -> times[1]
+    // hh -> times[0]
+
+    // console.log(`h in s: ${secondsPerHour*parseInt(times[0])}`);
+    // console.log(`m in s: ${secondsPerMin*parseInt(times[1])}`);
+    // console.log(`s in s: ${parseInt(times[2])}`);
+
+    return parseInt(
+            times[2]) + 
+            secondsPerMin * parseInt(times[1]) + 
+            secondsPerHour * parseInt(times[0]);
+}
+
+function preProcess (log) {
+    const logs = log.split('\n');
+    let map_number_seconds = new Map();
+    
+    for (let i=0; i<logs.length; i++) {
+        const number = logs[i].split(',')[1].replace(/-/g, '');
+        if (map_number_seconds.has(number)) {
+            const cumulativeTime = map_number_seconds.get(number);
+            const thisTime = countTime(logs[i].split(',')[0]);
+            map_number_seconds.set(number, cumulativeTime + thisTime);
+        } else {
+            map_number_seconds.set(number, countTime(logs[i].split(',')[0]));
+        }
+    }
+    return map_number_seconds;
+}
+
+const log = `00:01:07,400-234-090\n00:05:01,701-080-080\n00:05:00,400-234-090`;
+
+console.log(preProcess(log));
