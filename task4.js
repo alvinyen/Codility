92ce6a3030d14f6817ec1e1634706a5738d6ebb1
alvinyen <103356015@nccu.edu.tw>
2017-08-28 20:08:45 +0800

    complete listCombination function

--- task4.js
+++ task4.js
@@ -14,3 +14,31 @@ function combination (n, k) {
 // console.log(combination(5, 3));
 // console.log(combination(8, 3));
 // console.log(combination(3, 1));
+
+function getPrefixes (coordinatesArray) {
+    const k = 3;
+    let prefixes = [];
+    for (let fixed=0; fixed<=coordinatesArray.length-k; fixed++) {
+        for (let next=fixed+1; next<=coordinatesArray.length-k+1; next++) {
+            let temp = [];
+            temp.push(coordinatesArray[fixed], coordinatesArray[next]);
+            prefixes.push(temp);
+        }
+    }
+    
+    return prefixes;
+}
+
+function listCombination (coordinatesArray) {
+    const k = 3;
+    const prefixes = getPrefixes(coordinatesArray);
+    
+    console.log(prefixes);
+}
+
+let coordinatesArray = ['A', 'B', 'C', 'D', 'E'];
+listCombination(coordinatesArray);
+
+coordinatesArray = [{x: 15, y: 20}, {x: 16, y: 21}, {x: 17, y: 22}, {x: 18, y: 23}, {x: 19, y: 24}];
+listCombination(coordinatesArray);
+
