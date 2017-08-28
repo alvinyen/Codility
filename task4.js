99a1084cdd17a599187d4006278e60bc834a7bb3
alvinyen <103356015@nccu.edu.tw>
2017-08-28 19:53:21 +0800

    complete combination function

--- task4.js
+++ task4.js
@@ -6,3 +6,11 @@ function factorial (n) {
 // console.log(factorial(5));
 // console.log(factorial(4));
 // console.log(factorial(3));
+
+function combination (n, k) {
+    return factorial(n)/(factorial(n-k)*factorial(k));
+}
+
+// console.log(combination(5, 3));
+// console.log(combination(8, 3));
+// console.log(combination(3, 1));
